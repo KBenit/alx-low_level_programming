016c2db521b4df23a88cff3c2449f6b3e674fc85
KBenit <benitdeveloper@gmail.com>
2022-05-23 10:24:55 -0700

    test3

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,14 +12,14 @@ unsigned int binary_to_uint(const char *b);
 int i;
 unsigned int umu_b = 0;
 
- if (!b)
+ if (b == Null)
    return (0);
 
 for (i = 0; b[i]; i++)
        {
 	 if (b[i] > '1' || b[i] < '0')
 	   return (0);
-
+         umu_b = 2 * umu_b + (b[i] - '0');
        }
 
 return (umu_b);
